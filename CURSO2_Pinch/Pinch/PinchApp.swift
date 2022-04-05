de56efc523bf472ca9ab87a101cead9796dd2843
Carlos Alcala <carlos.alcala@me.com>
2022-04-05 18:42:15 -0400

    implemented code Pinch and Restart

---
+++ CURSO2_Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by skynet on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
