571da6032c14e1745446db1524366432df2f501e
Esteban Panda One <estebanpiro113@gmail.com>
2022-04-07 20:31:34 -0400

    New Project Fruits

---
+++ CURSO3_Fructus/Fructus/Fructus/Model/FruitModel.swift
@@ -0,0 +1,23 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by skynet on 7/4/22.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+    
+
+}
