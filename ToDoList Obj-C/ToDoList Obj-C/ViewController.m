713e77ff0388147fad8027053ca03fadbdc221ef
Esteban <estebanpiro113@gmail.com>
2022-04-19 14:17:32 -0400

    New Code

--- ToDoList Obj-C/ToDoList Obj-C/ViewController.m
+++ ToDoList Obj-C/ToDoList Obj-C/ViewController.m
@@ -7,7 +7,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIAlertViewDelegate>
 
 @property (nonatomic) NSMutableArray *items;
 
@@ -22,7 +22,30 @@
     
     self.items = @[@{@"name" : @"Take out the trash", @"category" : @"Home"}, @{@"name" : @"Go Shopping", @"category" : @"Home"}, @{@"name" : @"Go to work to App Boys", @"category" : @"Home"}].mutableCopy;
     self.navigationItem.title = @"To-do list";
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
 }
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Adding items
+
+- (void)addNewItem:(UIBarButtonItem *)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"New to-do item" message:@"Please enter the name of the new to-do item" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Add item", nil];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex != alertView.cancelButtonIndex) {
+        UITextField *itemNameField = [alertView textFieldAtIndex:0];
+        NSString *itemName = itemNameField.text;
+        NSDictionary *item = @{@"name" : itemName, @"category" : @"Home"};
+        [self.items addObject:item];
+        [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.items.count - 1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
 
 #pragma mark - Table view datasource
 - (NSInteger)numberOfSectionsInTable:(UITableView *)tableview {
